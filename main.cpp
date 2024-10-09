6db1bc0a95a55863a803e3f251391287151ea7a5
rafaellezama <107060188+rafaellezama@users.noreply.github.com>
2024-10-09 14:53:36 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "Rectangle.hpp"
 #include "Circle.hpp"
+#include "triangle.hpp"
 int main()
 {
   Shape *rect = new Rectangle(5, 5);
@@ -7,5 +8,13 @@ int main()
 
   Shape *circ = new Circle(5, 5);
   cout << "Circle Area: " << circ->getArea() << endl;
+
+  Shape *tri = new Triangle(3,4,3);
+  std::cout << "Triangle Area: " << tri->getArea() << std::endl;
+
+  delete rect;
+  delete tri;
+  delete circ;
+  
   return 0;
 }
