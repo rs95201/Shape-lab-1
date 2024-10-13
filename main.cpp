190db62ea6c425cbf372d7f9ea2a68699b7e4897
rs95201 <114031245+rs95201@users.noreply.github.com>
2024-10-12 22:10:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,16 +17,19 @@ int main()
   Rectangle r(2, 4);
   assert(r.getArea() == 8);
   assert(r.getPerimeter() == 12);
+  assert(r.getName() == "Rectangle");
   assert(printAreaToScreen(&r) == "Area of Rectangle is 8");
 
   Triangle t(3, 4, 3);
   assert(t.getArea() == 6);
   assert(t.getPerimeter() == 10);
+  assert(t.getName() == "Triangle");
   assert(printAreaToScreen(&t) == "Area of Triangle is 6");
 
   Isosceles i(4);
   assert(i.getArea() == 8);
   assert(i.getPerimeter() == 8 + (4 * sqrt(2)));
+  assert(i.getName() == "Isosceles Right Triangle");
   assert(printAreaToScreen(&i) == "Area of Isosceles Right Triangle is 8");
 
   // Add asserts for square and circle
@@ -37,10 +40,12 @@ int main()
   assert(printAreaToScreen(&s) == "Area of Square is 11.56");
 
   Circle c(5, 5);
-  assert(c.getArea() == M_PI * 5.5 * 5.5);
-  assert(c.getPerimeter() == 2 * M_PI * 5.5);
+  assert(c.getArea() == PI * 5.0 * 5.0);
+  assert(c.getPerimeter() == 2 * 5.0 * PI);
   assert(c.getName() == "Circle");
-  assert(printAreaToScreen(&i) == "Area of Circle is 78.54");
+  stringstream expected;
+  expected << "Area of Circle is " << PI * 5.0 * 5.0;
+  assert(printAreaToScreen(&c) == expected.str());
 
   std::cout
       << "All assertions passed" << std::endl;
